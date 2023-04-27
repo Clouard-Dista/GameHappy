db352aa509f011b22542dea185a6f70d8e46b690
Talloir Vincent <vincenttall@hotmail.fr>
2023-04-27 14:00:52 +0200

    add input management

---
+++ Intput.js
@@ -0,0 +1,48 @@
+const Input = (function () {
+    var constructeur = function () {
+        this.keyUpHandler = function (event) {
+            if (!Input.getInstance().lock) {
+                delete Input.getInstance().inputActive[event.code];
+            }
+        }
+        this.keyDownHandler = function (event) {
+            if (!Input.getInstance().lock){
+                Input.getInstance().inputActive[event.code] = true;
+                Input.getInstance().lastInput = event.code;
+                console.log(Input.getInstance().inputActive);
+            }
+        }
+        this.out = function (e) {
+            e = e ? e : window.event;
+            var from = e.relatedTarget || e.toElement;
+            if (!from || from.nodeName == "HTML") {
+                Input.getInstance().inputActive = {};
+                Input.getInstance().lock = true;
+            }
+        }
+        this.in = function (e) {
+            if (Input.getInstance().lock) {
+                Input.getInstance().lock = false;
+            }
+        }
+        this.inputActive = {};
+        this.lastInput = null;
+        this.lock = false;
+    }
+    let saveInstance = null;
+    return new function () {
+        this.getInstance = function () {
+            if (saveInstance == null) {
+                saveInstance = new constructeur();
+                saveInstance.constructeur = null;
+            }
+            return saveInstance;
+        }
+    }
+})();
+Input.getInstance()
+document.addEventListener("keydown", Input.getInstance().keyDownHandler, false);
+document.addEventListener("keyup", Input.getInstance().keyUpHandler, false);
+document.addEventListener("mouseout", Input.getInstance().out, false);
+document.addEventListener("mouseover", Input.getInstance().in, false);
+Input.getInstance().inputActive
